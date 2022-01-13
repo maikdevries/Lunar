d286bd4f1d2a4f9c4033fffedd0324b8a340525a
maikdevries <dev.maik.devries@outlook.com>
2022-01-13 16:38:27 +0100

    Close #103 - Create dashboard command

---
+++ src/commands/dashboard.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { dashboard } = require('../shared/messages.js');
+
+module.exports = {
+	memberPermissions: [],
+	guildPermissions: [],
+	channelPermissions: [],
+	data: new SlashCommandBuilder()
+		.setName('dashboard')
+		.setDescription('Launch into Orbit and manage Lunar\'s behaviour'),
+	execute
+}
+
+async function execute (interaction) {
+	return await interaction.reply({ content: dashboard(), ephemeral: true });
+}
